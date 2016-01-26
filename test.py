543480f895e46ccfefbd9fda226e48a0021f17b1
josemurillo2017 <josemurillo2017@gmail.com>
2016-01-25 22:30:10 -0600

    Update test.py
    Updated tests to reflect time component

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from simulation_functions import *
 
 jose = Person("{'Handsome': 100, 'Sexy':20}", "{'Handsome': 100, 'Sexy': 20}")
 jenny = Person("{'Handsome': 10, 'Sexy':200}", "{'Handsome': 100, 'Sexy': 200}")
-being_awesome = Event("Being Awesome", "YES!", ['Sexy'], "person.traits['Sexy']>10", "person.traits['Sexy'] +=10")
-being_handsome = Event("Being Handsome", "YES!", ['Handsome'], "person.traits['Sexy']>10", "person.traits['Handsome'] +=10")
+being_awesome = Event("Being Awesome", "YES!", ['Sexy'], "person.traits['Sexy']>10", "person.traits['Sexy'] +=10", 1)
+being_handsome = Event("Being Handsome", "YES!", ['Handsome'], "person.traits['Sexy']>10", "person.traits['Handsome'] +=10",1)
 world_zombie = World([being_awesome, being_handsome], [jose, jenny])
 simulate_event(world_zombie, jenny)
